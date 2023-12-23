ebe866e10ecde256cca4ed522103c7dcdd313a9f
Eliina0 <152660908+Eliina0@users.noreply.github.com>
2023-12-23 21:32:19 +0100

    Add files via upload

---
+++ projekt1/j1.js
@@ -0,0 +1,200 @@
+function get(){
+    var txt = document.getElementById('text').value.trim();
+    var list = document.getElementById('list');
+
+    if(txt !== '' && txt.length >= 2 && txt.length <= 50 && !/\d/.test(txt)){
+        if(!checkDuplicate(list , txt)){
+            var listItem = createListItem(txt);
+            var listItemData = {
+                text: txt,
+                done: false,
+                subItems: []
+            };
+            listItem.data = listItemData;
+            list.appendChild(listItem);
+            document.getElementById('text').value = '';
+            saveToLocalStorage(listItemData);
+
+        }else{
+            alert('Kjo detyrë është tashmë në listë.');
+        }
+    }else {
+        alert('Teksti duhet të jetë midis 2 dhe 50 karaktereve, të mos jetë bosh dhe të mos përmbajë numra.');
+    }
+}
+document.getElementById('add').addEventListener('click', get);
+
+function createListItem(txt){
+    var listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <input type='checkbox'>
+    ${txt}
+    <button class='btn-delete' onclick='deleteItem(this)'> Delete </button>
+    <button class='btn-add' onclick='addItem(this)'>Add </button> `;
+
+    listItem.querySelector('input[type="checkbox"]').addEventListener('change' , function(){
+        itemDone(this);
+    });
+    return listItem;
+}
+function itemDone(checkbox){
+    var listItem = checkbox.parentNode;
+    var listItemData = listItem.data;
+   
+    listItemData.done = checkbox.checked;
+
+    if(listItemData.subItems.length > 0){
+        listItemData.subItems.forEach(function(subitem){
+            subitem.done = listItemData.done;
+        });
+        listItem.querySelectorAll('.sub-item input[type="checkbox"]').forEach(function (subCheckbox) {
+            subCheckbox.checked = listItemData.done;
+        });     
+    }
+
+    updateItemInLocalStorage(listItemData);
+}
+function subitemDone(checkbox){
+    var listItem = checkbox.parentNode.parentNode;
+    var listItemData = listItem.data;
+    var subItemData = checkbox.parentNode.data;
+    subItemData.done = checkbox.checked;
+    var count = 0 ;
+    for (var i = 0 ; i < listItemData.subItems.length ; i++){
+        if(listItemData.subItems[i].done){
+            count++;
+        }
+    }
+    if(count === listItemData.subItems.length){
+        listItem.querySelector('input[type="checkbox"]').checked = true;
+        listItemData.done = true;
+    }
+    else{
+        listItem.querySelector('input[type="checkbox"]').checked = false;
+        listItemData.done = false;
+    }
+    updateItemInLocalStorage(listItemData);
+}
+function checkDuplicate(list , txt){
+    var items = document.getElementsByTagName('li');
+    for(var i = 0 ; i < items.length ; i++){
+        var itemText = items[i].data.text;
+        if(itemText === txt || checkDuplicateSubitem(items[i] , txt)){
+            return true;
+        }
+    } 
+    return false;
+}
+function checkDuplicateSubitem( item , txt){
+    var subitems = item.data.subItems || [];
+    console.log(item);
+    console.log(subitems);
+    for (var i = 0 ; i < subitems.length ; i++){
+        var subitemText = subitems[i].text;
+        if( subitemText === txt){
+            return true;
+        }
+    }
+    return false;
+}
+function saveToLocalStorage(itemData){
+    var doList = JSON.parse(localStorage.getItem('toDoList')) || [];
+    doList.push(itemData);
+    localStorage.setItem('toDoList' , JSON.stringify(doList));
+}
+function deleteItem(button){
+    var listItem = button.parentNode;
+    var listItemData = listItem.data;
+    var doList = JSON.parse(localStorage.getItem('toDoList'));
+    removeFromLocalStorage(doList , listItemData);
+    listItem.remove();
+}
+function removeFromLocalStorage(doList , listItemData){
+    var index = findIndexLocalStorage(doList , listItemData)
+   doList.splice(index , 1);
+   localStorage.setItem('toDoList' , JSON.stringify(doList));
+}
+function findIndexLocalStorage(doList , listItemData){
+    var txt = listItemData.text;
+    for(var i = 0 ; i < doList.length ; i++){
+        if(doList[i].text === txt){
+            return i;
+        }
+    }
+    return -1;
+}
+function addItem(button){
+    var listItemParent = button.parentNode;
+    var listItemParentData = listItemParent.data;
+    var txt = document.getElementById('text').value.trim();
+    if(txt !== '' && txt.length >= 2 && txt.length <= 50 && !/\d/.test(txt)){
+        if(!checkDuplicate(list , txt)){
+            var listItem = createSubitem(txt);
+            var listItemData = {
+                text: txt,
+                done: false
+            };
+            listItem.classList.add('sub-item');
+            listItem.data = listItemData;
+            listItemParent.appendChild(listItem);
+            listItemParentData.subItems.push(listItemData);
+            document.getElementById('text').value = '';
+            updateItemInLocalStorage(listItemParentData);
+
+        }else{
+            alert('Kjo detyrë është tashmë në listë.');
+        }
+    }else {
+        alert('Teksti duhet të jetë midis 2 dhe 50 karaktereve, të mos jetë bosh dhe të mos përmbajë numra.');
+    }
+}
+function createSubitem(txt){
+    var listItem = document.createElement('li');
+    listItem.innerHTML = `
+        <input type='checkbox'>
+        ${txt}
+        <button class='btn-delete' onclick='deleteSubitem(this)'>Delete</button> `;
+        listItem.querySelector('input[type="checkbox"]').addEventListener('change' , function(){
+            subitemDone(this);
+        });
+        return listItem;
+}
+function updateItemInLocalStorage(listItemData){
+    var doList = JSON.parse(localStorage.getItem('toDoList'));
+    let index = findIndexLocalStorage(doList , listItemData);
+    doList[index] = listItemData;
+    localStorage.setItem('toDoList' , JSON.stringify(doList));
+}
+function deleteSubitem(subitem) {
+    var listItem = subitem.parentNode.parentNode; 
+    var listItemData = listItem.data;
+    var subItemData = subitem.parentNode.data;
+    listItemData.subItems = listItemData.subItems.filter(function(item){
+        return item.text !== subItemData.text;
+    }); 
+    var doList = JSON.parse(localStorage.getItem('toDoList'));
+    updateItemInLocalStorage(listItemData);
+    subitem.parentNode.remove();
+}
+function fromLocalStorage() {
+    var doList = JSON.parse(localStorage.getItem('toDoList')) || [];
+    var list = document.getElementById('list')
+    
+    doList.forEach(function (itemData) {
+        var listItem = createListItem(itemData.text);
+
+        listItem.data = itemData;
+        list.appendChild(listItem);
+        listItem.querySelector('input[type="checkbox"]').checked = itemData.done;
+        if (itemData.subItems.length > 0) {
+            itemData.subItems.forEach(function (subitem) {
+                var listSubitem = createSubitem(subitem.text);
+                listSubitem.classList.add('sub-item');
+                listSubitem.data = subitem;
+                listSubitem.querySelector('input[type="checkbox"]').checked = subitem.done;
+                listItem.appendChild(listSubitem);
+            });
+        }
+    });
+}
+fromLocalStorage();
